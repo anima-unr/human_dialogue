ad7126929a4595cf3d9d461bec70294e4368328e
Janelle <jjblankenburg@nevada.unr.edu>
2017-05-08 18:34:23 -0700

    Converted from forward kinematics via Baxter API to moveit

--- unr_object_manipulation/baxter_table_setting/scripts/pick_place_service.py
+++ unr_object_manipulation/baxter_table_setting/scripts/pick_place_service.py
@@ -36,8 +36,16 @@ from baxter_core_msgs.srv import (
 
 from unr_manipulation.srv import *
 
+import moveit_commander
+import moveit_msgs.msg
+
 import threading as thread
 import pdb
+
+# TODO: JB Added
+import moveit_commander
+import moveit_msgs.msg
+import roslib
 
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
@@ -46,23 +54,31 @@ def enum(*sequential, **named):
 STATE = enum('APPROACHING', 'PICKING', 'PICKED', 'PLACING', 'PLACED', 'NEUTRAL', 'IDLE')
 
 class PickPlace(object):
-    def __init__(self, limb):
-        self.side = limb
-        self._limb = baxter_interface.Limb(limb)
+    def __init__(self, limb, side):
+        # TODO: Replace limb motion with moveit motions
+        self.side = side
+        # TODO: JB Added
+        # self._limb = baxter_interface.Limb(limb)
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self._limb = moveit_commander.MoveGroupCommander(limb)
 
-        circle_io = baxter_interface.DigitalIO(limb + '_lower_button')
-        dash_io = baxter_interface.DigitalIO(limb + '_upper_button')
+        circle_io = baxter_interface.DigitalIO(side + '_lower_button')
+        dash_io = baxter_interface.DigitalIO(side + '_upper_button')
         self.calibrating = False
         self.object_calib = -1
         # self.objects = ['neutral', 'placemat', 'cup', 'plate', 'fork', 'spoon', 'knife', 'bowl', 'soda', 'wineglass']
-        self.objects = ['neutral',  'cup', 'plate', 'bowl']
-        # self.objects = ['neutral', 'cup']
-        self.object_pick_joint_angles = dict()
+        # self.objects = ['neutral',  'cup', 'plate', 'bowl']
+        self.objects = ['neutral', 'bowl']
+        # TODO: JB Added
+        # self.object_pick_joint_angles = dict()
         self.object_pick_poses = dict()
-        self.object_place_joint_angles = dict()
+        # TODO: JB Added
+        # self.object_place_joint_angles = dict()
         self.object_place_poses = dict()
 
-        self._gripper = baxter_interface.Gripper(limb)
+
+        self._gripper = baxter_interface.Gripper(side)
         self._gripper.calibrate()
         self._gripper.set_holding_force(100.0)
 
@@ -83,27 +99,95 @@ class PickPlace(object):
         if value and self.object_calib != -1:
             if self.pick:
                 print "Calibrating Object Pick: " + self.objects[self.object_calib] + "..."
-                self.object_pick_joint_angles[self.objects[self.object_calib]] = self._limb.joint_angles()
-                self.object_pick_poses[self.objects[self.object_calib]] = self._limb.endpoint_pose()
+                # TODO: JB Added
+                # self.object_pick_joint_angles[self.objects[self.object_calib]] = self._limb.joint_angles()
+                # self.object_pick_poses[self.objects[self.object_calib]] = self._limb.endpoint_pose()
+                pose = self.GetArmPoseGoal()
+                self.object_pick_poses[self.objects[self.object_calib]] = pose
                 print "Calibrated"
                 self.calibrating = False
             else:
-                print "Calibrating Object Place: " + self.objects[self.object_calib] + "..."
-                self.object_place_joint_angles[self.objects[self.object_calib]] = self._limb.joint_angles()
-                self.object_place_poses[self.objects[self.object_calib]] = self._limb.endpoint_pose()
+                # TODO: JB Added
+                # self.object_place_joint_angles[self.objects[self.object_calib]] = self._limb.joint_angles()
+                # self.object_place_poses[self.objects[self.object_calib]] = self._limb.endpoint_pose()
+                pose = self.GetArmPoseGoal()
+                self.object_place_poses[self.objects[self.object_calib]] = pose
                 print "Calibrated"
                 self.calibrating = False
+
+
+    # TODO: JB Added
+    def GetArmPoseGoal(self):
+
+        # set up the things
+        pose_target = Pose()
+        now = rospy.Time(0)
+
+        # get the tf
+        t = tf.TransformListener(True, rospy.Duration(10.0))
+        t.waitForTransform("/base", "/right_gripper", now,rospy.Duration(3.0));
+        pose = t.lookupTransform("/base", "/right_gripper", now)
+
+        # format pose to correct format
+        pose_target.orientation.x = pose[1][0]
+        pose_target.orientation.y = pose[1][1]
+        pose_target.orientation.z = pose[1][2]
+        pose_target.orientation.w = pose[1][3]
+        pose_target.position.x = pose[0][0] 
+        pose_target.position.y = pose[0][1] 
+        pose_target.position.z = pose[0][2]
+
+        self._limb.set_pose_target(pose_target)
+        self._limb.set_num_planning_attempts(3);
+        self._limb.set_planning_time(5.0);
+        self._limb.set_goal_position_tolerance(0.1)
+        self._limb.set_goal_orientation_tolerance(0.1)
+
+        return pose_target
+
+
+    def moveToPose(self, pose):
+
+        # define temp pose
+        pose_target = Pose()
+
+        # format the pose correctly
+        print pose
+        pose_target.orientation.x = pose.orientation.x
+        pose_target.orientation.y = pose.orientation.y
+        pose_target.orientation.z = pose.orientation.z
+        pose_target.orientation.w = pose.orientation.w
+        pose_target.position.x = pose.orientation.x
+        pose_target.position.y = pose.orientation.y
+        pose_target.position.z = pose.orientation.z
+
+        # set things
+        self._limb.set_pose_target(pose_target)
+        self._limb.set_num_planning_attempts(5);
+        self._limb.set_planning_time(10.0);
+        self._limb.set_goal_position_tolerance(0.1)
+        self._limb.set_goal_orientation_tolerance(0.1)
+
+        print("\tPlanning...")
+        plan1 = self._limb.plan()
+        rospy.sleep(5)
+        print("\tExecuting...")
+        self._limb.go(wait=True)
+        rospy.sleep(5)
+
 
     def PickAndPlaceImpl(self, req):
 
         if self.stop:
             return
         print "Picking UP Object: " + req.object
-        self._limb.set_joint_position_speed(0.2)
+        # self._limb.set_joint_position_speed(0.2)
         if self.stop:
             return
         self.state = STATE.NEUTRAL
-        self._limb.move_to_joint_positions(self.object_pick_joint_angles['neutral'])
+        # TODO: JB Added
+        # self._limb.move_to_joint_positions(self.object_pick_joint_angles['neutral'])
+        self.moveToPose(self.object_pick_poses['neutral'])
         if self.stop:
             return
         self._gripper.command_position(100.0)
@@ -114,7 +198,8 @@ class PickPlace(object):
         if self.stop:
             return
         self.state = STATE.PICKING
-        self._limb.move_to_joint_positions(self.object_pick_joint_angles[req.object])
+        # self._limb.move_to_joint_positions(self.object_pick_joint_angles[req.object])
+        self.moveToPose(self.object_pick_poses[req.object])
         if self.stop:
             return
         self._gripper.command_position(0.0)
@@ -124,17 +209,20 @@ class PickPlace(object):
         rospy.sleep(0.4)
         if self.stop:
             return
-        self._limb.move_to_joint_positions(self.object_pick_joint_angles['neutral'])
+        # self._limb.move_to_joint_positions(self.object_pick_joint_angles['neutral'])
+        self.moveToPose(self.object_pick_poses['neutral'])
         if self.stop:
             return
         print "Placing Down Object:" + req.object
         self.state = STATE.PLACING
         if self.stop:
             return
-        self._limb.move_to_joint_positions(self.object_place_joint_angles['neutral'])
+        # self._limb.move_to_joint_positions(self.object_place_joint_angles['neutral'])
+        self.moveToPose(self.object_place_poses['neutral'])
         if self.stop:
             return
-        self._limb.move_to_joint_positions(self.object_place_joint_angles[req.object])
+        # self._limb.move_to_joint_positions(self.object_place_joint_angles[req.object])
+        self.moveToPose(self.object_place_poses[req.object])
         if self.stop:
             return
         self._gripper.command_position(100.0)
@@ -144,13 +232,30 @@ class PickPlace(object):
         rospy.sleep(0.4)
         if self.stop:
             return
-        self._limb.move_to_joint_positions(self.object_place_joint_angles['neutral'])
+        # self._limb.move_to_joint_positions(self.object_place_joint_angles['neutral'])
+        self.moveToPose(self.object_place_poses['neutral'])
         if self.stop:
             return
         self._gripper.command_position(0.0)
         if self.stop:
             return
         self.state = STATE.IDLE
+
+    def PickAndPlaceSendGoal(self):
+        display_trajectory_publisher = rospy.Publisher(
+                            '/move_group/display_planned_path',
+                            moveit_msgs.msg.DisplayTrajectory)
+        # THE WAY LUKE DID IT:
+        # self._limb.move_to_joint_positions(self.object_pick_joint_angles['neutral'])
+
+        # TODO: Replace limb motion with moveit motions
+        # pose_target = geometry_msgs.msg.Pose()
+        # pose_target.orientation.w = 1.0
+        # pose_target.position.x = 0.7
+        # pose_target.position.y = -0.05
+        # pose_target.position.z = 1.1
+        # group.set_pose_target(pose_target)
+        # plan1 = group.plan()
 
     def PickAndPlaceObject(self, req):
         # starting a thread that will handle the pick and place.
@@ -240,40 +345,130 @@ class PickPlace(object):
         resp = self._iksvc(ikreq)
         return dict(zip(resp.joints[0].name, resp.joints[0].position))
 
+    # TODO: JB Added
+    # def ReadCalibration(self, filename):
+    #     with open(filename, 'r') as f:
+    #         for line in f:
+    #             split = line.split('%')
+    #             location = split[0]
+    #             pick_or_place = location.split('_')[0]
+    #             key = location.split('_')[1]
+    #             position = split[1]
+    #             if pick_or_place == 'pickAngles':
+    #                 self.object_pick_joint_angles[key] = eval(position)
+    #             elif pick_or_place == 'pickPose':
+    #                 self.object_pick_poses[key] = eval(position)
+    #             elif pick_or_place == 'placeAngles':
+    #                 self.object_place_joint_angles[key] = eval(position)
+    #             elif pick_or_place == 'placePose':
+    #                 self.object_place_poses[key] = eval(position)
     def ReadCalibration(self, filename):
         with open(filename, 'r') as f:
-            for line in f:
-                split = line.split('%')
-                location = split[0]
-                pick_or_place = location.split('_')[0]
-                key = location.split('_')[1]
-                position = split[1]
-                if pick_or_place == 'pickAngles':
-                    self.object_pick_joint_angles[key] = eval(position)
-                elif pick_or_place == 'pickPose':
-                    self.object_pick_poses[key] = eval(position)
-                elif pick_or_place == 'placeAngles':
-                    self.object_place_joint_angles[key] = eval(position)
-                elif pick_or_place == 'placePose':
-                    self.object_place_poses[key] = eval(position)
+            # read base frame id and write end-effector frame id? -- not used so ignore!?!
+            line = f.readline()
+            base_frame_id = line.rstrip()
+            line = f.readline()
+            ee_frame_id = line.rstrip()
+            print(base_frame_id)
+            print(ee_frame_id)
 
-    def SaveCalibration(self, filename):
-        f = open(filename, 'w')
-        for key in self.object_pick_joint_angles:
-            f.write('pickAngles_' + key + '%' + str(self.object_pick_joint_angles[key]) + '\n')
-            f.write('pickPose_' + key + '%' + str(self.object_pick_poses[key]) + '\n')
-        for key in self.object_place_joint_angles:
-            f.write('placeAngles_' + key + '%' + str(self.object_place_joint_angles[key]) + '\n')
-            f.write('placePose_' + key + '%' + str(self.object_place_poses[key]) + '\n')
+            # until end of file
+            while True:
+                line = f.readline()
+                if not line: 
+                    break
+
+                # read key
+                key = line.rstrip()
+
+                # read into pick pos at key
+                line = f.readline()
+                line = line.rstrip()                
+                data = line.split(',')
+                self.object_pick_poses[key] = Pose()
+                self.object_pick_poses[key].position.x = float(data[0])
+                self.object_pick_poses[key].position.y = float(data[1])
+                self.object_pick_poses[key].position.z = float(data[2])
+
+                # read into pick ori at key
+                line = f.readline()
+                line = line.rstrip()
+                data = line.split(',')
+                self.object_pick_poses[key].orientation.x = float(data[0])
+                self.object_pick_poses[key].orientation.y = float(data[1])
+                self.object_pick_poses[key].orientation.z = float(data[2])
+                self.object_pick_poses[key].orientation.w = float(data[3])
+
+                # read into place pos at key
+                line = f.readline()
+                line = line.rstrip()
+                data = line.split(',')
+                self.object_place_poses[key] = Pose()
+                self.object_place_poses[key].position.x = float(data[0])
+                self.object_place_poses[key].position.y = float(data[1])
+                self.object_place_poses[key].position.z = float(data[2])
+
+                # read into place ori at key
+                line = f.readline()
+                line = line.rstrip()
+                data = line.split(',')
+                self.object_place_poses[key].orientation.x = float(data[0])
+                self.object_place_poses[key].orientation.y = float(data[1])
+                self.object_place_poses[key].orientation.z = float(data[2])
+                self.object_place_poses[key].orientation.w = float(data[3])
+
+                print key
+                print self.object_pick_poses[key]
+
         f.close()
 
+
+    # TODO: JB Added
+    # def SaveCalibration(self, filename):
+    #     f = open(filename, 'w')
+    #     for key in self.object_pick_joint_angles:
+    #         f.write('pickAngles_' + key + '%' + str(self.object_pick_joint_angles[key]) + '\n')
+    #         f.write('pickPose_' + key + '%' + str(self.object_pick_poses[key]) + '\n')
+    #     for key in self.object_place_joint_angles:
+    #         f.write('placeAngles_' + key + '%' + str(self.object_place_joint_angles[key]) + '\n')
+    #         f.write('placePose_' + key + '%' + str(self.object_place_poses[key]) + '\n')
+    #     f.close()
+    def SaveCalibration(self, filename):
+        f = open(filename, 'w')
+        # write base frame id and write end-effector frame id?
+        f.write('base\n' + self.side + '_gripper' + '\n')
+
+        for key in self.object_pick_poses:
+            # write object name
+            f.write(key + '\n')
+            # write pick pos
+            f.write(str(self.object_pick_poses[key].position.x) + ',' +
+                str(self.object_pick_poses[key].position.y) + ',' +
+                str(self.object_pick_poses[key].position.z) +'\n')
+            # write pick ori
+            f.write(str(self.object_pick_poses[key].orientation.x) + ',' +
+                str(self.object_pick_poses[key].orientation.y) + ',' +
+                str(self.object_pick_poses[key].orientation.z) + ',' + 
+                str(self.object_pick_poses[key].orientation.w) + '\n')
+            # write place pos
+            f.write(str(self.object_place_poses[key].position.x) + ',' +
+                str(self.object_place_poses[key].position.y) + ',' +
+                str(self.object_place_poses[key].position.z) +'\n')
+            # write place ori
+            f.write(str(self.object_place_poses[key].orientation.x) + ',' +
+                str(self.object_place_poses[key].orientation.y) + ',' +
+                str(self.object_place_poses[key].orientation.z) + ',' + 
+                str(self.object_place_poses[key].orientation.w) + '\n')
+        f.close()
+
+    # TODO: JB Added
     def PostParameters(self):
         for key in self.object_pick_poses:
             # Post param for Pick Position
             rospy.set_param('/ObjectPositions/' + key,
-                [self.object_pick_poses[key]['position'].x,
-                 self.object_pick_poses[key]['position'].y,
-                 self.object_pick_poses[key]['position'].z])
+                [self.object_pick_poses[key].position.x,
+                 self.object_pick_poses[key].position.y,
+                 self.object_pick_poses[key].position.z])
 
 
 def main():
@@ -287,7 +482,7 @@ def main():
     rs = baxter_interface.RobotEnable()
     print("Enabling Robot")
     rs.enable()
-    pp = PickPlace('right')
+    pp = PickPlace('right_arm', 'right')
 
     # Calibrate Pickup Locations
     if args.read:
